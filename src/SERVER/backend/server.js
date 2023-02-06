4ceaeb12104610811d2b912c2d22d2081f00419f
Hasankib <hasankibria00@gmail.com>
2023-02-05 20:15:26 -0500

    filter recipes and return results

--- src/SERVER/backend/server.js
+++ src/SERVER/backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 const user = require("./routes/userRoutes");
+const recipeFilters = require("./routes/recipeFilterRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -9,5 +10,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use("/users", user);
+app.use('/recipefilter', recipeFilters)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
